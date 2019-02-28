045ad9bcd098860367b7138cf0494ecef5ed0cdf
blakeanedved <blakeanedved@gmail.com>
2019-02-28 10:40:05 -0600

    Updated cards, and added a real banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $.getJSON('https://api.github.com/users/blakeanedved/repos',function(data){
-	for (var i = 0; i < data.length; i++){
+	data = data.sort(function(a, b){
+		return new Date(b.updated_at) - new Date(a.updated_at)
+	})
+	for (var i = 0; i < data.length && i < 16; i++){
 		$('.latest-projects.one').append(`<div class="col s3">
 			<div class="card small">
 				<div class="card-content">
@@ -10,35 +13,39 @@ $.getJSON('https://api.github.com/users/blakeanedved/repos',function(data){
 				<a class="source-link" href="${data[i].html_url}">Source</a>
 			</div>
 		</div>`)
-		$('.latest-projects.two').append(`<div class="col s4">
-			<div class="card small">
-				<div class="card-content">
-					<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
-					<span class="lang blue white-text">${data[i].language}</span>
-					<p class="card-content-custom">${data[i].description}</p>
+		if (i < 12){
+			$('.latest-projects.two').append(`<div class="col s4">
+				<div class="card small">
+					<div class="card-content">
+						<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
+						<span class="lang blue white-text">${data[i].language}</span>
+						<p class="card-content-custom">${data[i].description}</p>
+					</div>
+					<a class="source-link" href="${data[i].html_url}">Source</a>
 				</div>
-				<a class="source-link" href="${data[i].html_url}">Source</a>
-			</div>
-		</div>`)
-		$('.latest-projects.three').append(`<div class="col s6">
-			<div class="card small">
-				<div class="card-content">
-					<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
-					<span class="lang blue white-text">${data[i].language}</span>
-					<p class="card-content-custom">${data[i].description}</p>
+			</div>`)
+		}
+		if (i < 8){
+			$('.latest-projects.three').append(`<div class="col s6">
+				<div class="card small">
+					<div class="card-content">
+						<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
+						<span class="lang blue white-text">${data[i].language}</span>
+						<p class="card-content-custom">${data[i].description}</p>
+					</div>
+					<a class="source-link" href="${data[i].html_url}">Source</a>
 				</div>
-				<a class="source-link" href="${data[i].html_url}">Source</a>
-			</div>
-		</div>`)
-		$('.latest-projects.four').append(`<div class="col s12">
-			<div class="card small">
-				<div class="card-content">
-					<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
-					<span class="lang blue white-text">${data[i].language}</span>
-					<p class="card-content-custom">${data[i].description}</p>
+			</div>`)
+			$('.latest-projects.four').append(`<div class="col s12">
+				<div class="card small">
+					<div class="card-content">
+						<a href="${data[i].html_url}" class="black-text card-title"><b>${data[i].name}</b></a>
+						<span class="lang blue white-text">${data[i].language}</span>
+						<p class="card-content-custom">${data[i].description}</p>
+					</div>
+					<a class="source-link" href="${data[i].html_url}">Source</a>
 				</div>
-				<a class="source-link" href="${data[i].html_url}">Source</a>
-			</div>
-		</div>`)
+			</div>`)
+		}
 	}
 })
